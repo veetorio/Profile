b20dbb05fb9258a7cab4e4fe01b99236af1b5e7d
Veetorio <ettorevitorio98@gmail.com>
2024-06-09 20:14:57 -0300

    first commit

--- src/assets/components/main/Section.tsx
+++ src/assets/components/main/Section.tsx
@@ -2,14 +2,29 @@ import { InputType, RequestRepo } from "../../Interfaces/Types";
 import Project from "./content/Project";
 import Title from "./title/Title";
 import Input from "../Input/Input";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { GitHubRepositoriesDefault } from "../../Interfaces/GitGet";
 import { FaSearch } from "react-icons/fa";
 import style from '../../../../public/style/scroll.module.css'
 
-const items: RequestRepo[] = await GitHubRepositoriesDefault();
+
+
+async function Repos(): Promise<RequestRepo[]> {
+  return await GitHubRepositoriesDefault();
+}
 function Section() {
   const _input1 = useRef<HTMLInputElement>(null)
+
+  const [repos, setRepos] = useState<RequestRepo[]>([]);
+
+  useEffect(() => {
+    async function loadRepos() {
+      const reposData = await Repos();
+      setRepos(reposData);
+    }
+
+    loadRepos();
+  }, []);
 
   _input1.current?.addEventListener("keypress", () => {
   });
@@ -20,11 +35,11 @@ function Section() {
         <div className={`flex items-center py-4 shadow-2xl ${style.custom}`}>
           <Input tag={_input1} place="nome do repositorio" type={InputType.TEXT} />
           <Input tag={_input1} place="nome do repositorio" type={InputType.NUM} />
-          <button className="h-10 w-10 flex items-center justify-center rounded-md bg-slate-950"><FaSearch/></button>
+          <button className="h-10 w-10 flex items-center justify-center rounded-md bg-slate-950"><FaSearch /></button>
         </div>
       </div>
       <div className="h-96 overflow-y-auto">
-        {items?.map((e) => {
+        {repos.map((e) => {
           return <Project title={e.name} desc={e.description} url={e.html_url} />;
         })}
       </div>
