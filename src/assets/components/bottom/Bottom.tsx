6af48159488712ac3617f3b586468fef7500ed61
Veetorio <ettorevitorio98@gmail.com>
2024-06-09 20:27:55 -0300

    first commit

--- src/assets/components/bottom/Bottom.tsx
+++ src/assets/components/bottom/Bottom.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
     const {icons} = props;
     return (
-        <div className={`bg-[url(/public/svg/waveGithub.svg)] bg-neutral-900 bg-cover flex items-center justify-center shadow-2xl border-black border-3 h-1/3 w-1/6 rounded-lg max-[640px]:h-28 max-[640px]:w-24`}>
+        <div className={`bg-[url(../../../../public/svg/waveGithub.svg)] bg-neutral-900 bg-cover flex items-center justify-center shadow-2xl border-black border-3 h-1/3 w-1/6 rounded-lg max-[640px]:h-28 max-[640px]:w-24`}>
             {icons}  
         </div>
     )
