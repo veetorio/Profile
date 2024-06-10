e8514e5646153540ea15765ffbf295d634a42741
Veetorio <ettorevitorio98@gmail.com>
2024-06-09 22:41:58 -0300

    new add

--- src/assets/components/bottom/Bottom.tsx
+++ src/assets/components/bottom/Bottom.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react";
-import wave from '../../../../public/svg/waveGitHub.svg'
+import wave from './bg.module.css'
+console.log(wave)
 
  function Bottom(props : { icons : ReactNode}){
 
     const {icons} = props;
     return (
-        <div className={`bg-[url(${wave})] bg-neutral-900 bg-cover flex items-center justify-center shadow-2xl border-black border-3 h-1/3 w-1/6 rounded-lg max-[640px]:h-28 max-[640px]:w-24`}>
+        <div className={` ${wave['bg-fill']} bg-neutral-900 bg-cover flex items-center justify-center shadow-2xl border-black border-3 h-1/3 w-1/6 rounded-lg max-[640px]:h-28 max-[640px]:w-24`}>
             {icons}  
         </div>
     )
